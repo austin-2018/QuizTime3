068c96505e4e9a0288749e076eff0185f6486161
Victor <victor.n.tran@outlook.com>
2018-02-23 17:36:38 -0500

    Implemented StartQuiz and GradeQuiz functions. Works fine with all types of question objects

--- QuizTime3/CheckBox.cs
+++ QuizTime3/CheckBox.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuizTime3
 {
     class CheckBox : Question
     {
+        public static List<char> MultipleAnswers { get; set; }
+
         public CheckBox(int numberOfPossibleAnswer) : base(numberOfPossibleAnswer)
         {
+            MultipleAnswers = new List<char>();
+        }
 
+        protected override void SetCorrectAnswer()
+        {
+            Console.Clear();
+            Utility.PrintQuestionOut("Choose which of the following answers are correct: ", Answers);
+            GetChoices();
+            foreach (char character in MultipleAnswers)
+            {
+                Answer correctanswer = (Answers.Single(individualAnswer => individualAnswer.ID.Equals(int.Parse(character.ToString()))));
+                correctanswer.IsCorrectAnswer = true;
+            }
+        }
+
+        private void GetChoices()
+        {
+            string input;
+            do
+            {
+                Console.Write("Choice [separate by spaces]: ");
+                input = Console.ReadLine();
+            } while (!IsValidSelection(input, Answers));
+
+            foreach (char character in input)
+            {
+                if(!(character.Equals(' ')))
+                {
+                    MultipleAnswers.Add(character);
+                }
+            }
+        }
+
+        private bool IsValidSelection(string input, List<Answer> answers)
+        {
+            bool result = true;
+            string[] inputSplit = input.Split(' ');
+            if (result)
+            {
+                if (!(inputSplit.Length > answers.Count))
+                {
+                    for (int i = 0; i < inputSplit.Length; i++)
+                    {
+                        result = true && (int.Parse(inputSplit[i]) > 0 || int.Parse(inputSplit[i]) < answers.Count) ? true : false;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
--- QuizTime3/Quiz.cs
+++ QuizTime3/Quiz.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuizTime3
@@ -7,36 +8,57 @@ namespace QuizTime3
     class Quiz
     {
         public string Name { get; set; }
-        private  List<Question> Questions { get; set; }
+        private static List<Question> Questions { get; set; }
         private static List<string> QuestionTypesStringVersion = new List<string>
         {
             "Multiple Choice",
             "True and False",
             "Check Box - Multiple Choice"
         };
+        private static List<Answer> Answers { get; set; }
 
         public Quiz()
         {
             Console.Write("Enter name for this Quiz: ");
             Name = Console.ReadLine();
             Questions = new List<Question>();
+            Answers = new List<Answer>();
         }
 
         public void Play()
         {
+            SetUpQuestions();
+            StartQuiz();
+            Console.WriteLine(GradeQuiz());
+        }
+
+        public void SetUpQuestions()
+        {
             int questionAmount = DetermineQuestionAmount();
             for (int i = 0; i < questionAmount; i++)
             {
-                SetUpQuestion();
+                Console.Clear();
+                Utility.PrintQuestionOut("What kind of question do you want this question to be: ", QuestionTypesStringVersion);
+                Questions.Add(DetermineQuestionType());
             }
-            
         }
 
-        public void SetUpQuestion()
+        public void StartQuiz()
+        {
+            foreach (Question question in Questions)
+            {
+                Utility.PrintQuestionOut(question.Name, question.Answers);
+                int choice = int.Parse(Utility.GetChoice(question.Answers));
+                Answers.Add(question.Answers.Single(answer => answer.ID.Equals(choice)));
+            }
+        }
+
+        public string GradeQuiz()
         {
             Console.Clear();
-            Utility.PrintQuestionOut("What kind of question do you want this question to be: ", QuestionTypesStringVersion);
-            Questions.Add(DetermineQuestionType());
+            List<Answer> correct = Answers.FindAll(answer => answer.IsCorrectAnswer);
+            double result = (double) correct.Count / (double) Answers.Count;
+            return string.Format("{0} out of {1} correct: {2:P}", correct.Count, Answers.Count, result);
         }
 
         public int DetermineQuestionAmount()
@@ -55,7 +77,7 @@ namespace QuizTime3
 
         public static Question MakeQuestionType(string key)
         {
-            return key.Equals("1") ? new MultipleChoice(4) as Question: key.Equals("2") ? new TrueFalse(2) as Question: new CheckBox(5) as Question;
+            return key.Equals("1") ? new MultipleChoice(4) as Question: key.Equals("2") ? new TrueFalse() as Question: new CheckBox(5) as Question;
         }
     }
 }
