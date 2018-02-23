0598b8d283121dd5edbd3f50e9ff2f627546226c
Victor <victor.n.tran@outlook.com>
2018-02-23 17:34:44 -0500

    Updated True/False to have default True/False answer objects

--- QuizTime3/TrueFalse.cs
+++ QuizTime3/TrueFalse.cs
@@ -6,9 +6,15 @@ namespace QuizTime3
 {
     class TrueFalse : Question
     {
-        public TrueFalse(int numberOfPossibleAnswers) : base(numberOfPossibleAnswers)
+        public TrueFalse() : base()
         {
-
+            int nextAnswerId = 1;
+            Answers = new List<Answer>
+            {
+                new Answer(nextAnswerId++, "true"),
+                new Answer(nextAnswerId, "false")
+            };
+            SetCorrectAnswer();
         }
     }
 }
