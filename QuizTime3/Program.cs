824e9b4c3788c1829007191b104459fd97d1cef7
Victor <victor.n.tran@outlook.com>
2018-02-24 14:08:54 -0500

    Implement Game object running the game

--- QuizTime3/Program.cs
+++ QuizTime3/Program.cs
@@ -6,14 +6,25 @@ namespace QuizTime3
     {
         static void Main(string[] args)
         {
-            SetUp();
+            Game game = new Game();
+            Quiz quiz;
+            while (game.IsRunning)
+            {
+                quiz = new Quiz();
+                quiz.Play();
+                PlayAgain(game);
+
+            }
             Console.ReadLine();
         }
 
-        static void SetUp()
+
+        private static void PlayAgain(Game game)
         {
-            Quiz quiz = new Quiz();
-            quiz.Play();
+            string choice = Utility.GetAnswer("Do you want to play again ?", game);
+            Option pickedOption = game.Options.Find(option => option.ID.Equals(int.Parse(choice)));
+            game.IsRunning = pickedOption.Name.Equals("Yes");
+            Console.Clear();
         }
     }
 }
