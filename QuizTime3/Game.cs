05fd6908a2ca3c0608b01811fefa4121ac67a312
Victor <victor.n.tran@outlook.com>
2018-02-24 14:08:10 -0500

    Made a game class that will have the user decide if they want to keep playing

---
+++ QuizTime3/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizTime3
+{
+    class Game
+    {
+        public bool IsRunning { get; set; }
+        public List<Option> Options { get; set; }
+
+        public Game()
+        {
+            IsRunning = true;
+            Options = new List<Option>
+            {
+                new Option("Yes"),
+                new Option("No")
+            };
+        }
+    }
+}
---
+++ QuizTime3/Option.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizTime3
+{
+    class Option
+    {
+        private static int nextID = 0; 
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Option(string name)
+        {
+            ID = ++nextID;
+            Name = name;
+        }
+    }
+}
